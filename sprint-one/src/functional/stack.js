b7fa031a1cdfb7c37ad41b09271b690e9bd88b60
lain.lai.jiang@gmail.com <lain.lai.jiang@gmail.com>
2015-08-19 18:37:24 -0500

    Implemented and passed all tests on a functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,4 +1,5 @@
 var Stack = function(){
+  var count = 0;
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -6,13 +7,22 @@ var Stack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[count] = value;  
+    count++;  
   };
 
   someInstance.pop = function(){
+    if(count > 0){
+      count--; 
+    } 
+    var holder = storage[count]; 
+    delete storage[count];
+    return holder;
   };
 
   someInstance.size = function(){
+    return count;
   };
-
   return someInstance;
 };
+
