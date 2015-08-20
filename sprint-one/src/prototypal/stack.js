d6b5c695e6ba7d02c509a81b04d0a8f4188189d7
lain.lai.jiang@gmail.com <lain.lai.jiang@gmail.com>
2015-08-20 12:51:43 -0500

    Implemented and passed all tests on stack with prototypal instantiation

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.count = 0;
+  
+  return someInstance;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push : function(value){
+    this.storage[this.count] = value;
+    this.count++;
+  },
+  pop : function(){
+    if (this.count > 0){
+      this.count--;
+    }
+    var holder = this.storage[this.count];
+    delete this.storage[this.count];
+    return holder;
+  },
+  size : function(){
+    return this.count;
+  }
+};
