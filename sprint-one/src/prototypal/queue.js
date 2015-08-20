b044223386f392c7bf0bfd284cd0b6e4cdc11727
lain.lai.jiang@gmail.com <lain.lai.jiang@gmail.com>
2015-08-20 13:01:42 -0500

    Implmented and passed all tests on queue with prototypal instantiation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,36 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  someInstance.eCnt = 0;
+  someInstance.dCnt = 0;
+
+  return someInstance;
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value){
+    this.storage[this.eCnt] = value;
+    this.eCnt++;
+
+  },
+  dequeue : function(){
+    if(this.eCnt - this.dCnt > 0){
+      
+      var holder = this.storage[this.dCnt];
+      delete this.storage[this.dCnt];
+      this.dCnt++;
+      return holder;
+    }
+
+  },
+  size: function(){
+    return this.eCnt - this.dCnt;
+  }
+
+
+};
 
 
