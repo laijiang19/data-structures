c01af1a3a5b389e7fbc7722e3be93d16dcb4039f
lain.lai.jiang@gmail.com <lain.lai.jiang@gmail.com>
2015-08-19 23:26:04 -0500

    Implemented and passed all tests on queue with functional instantiation

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,35 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var enqueueCount = 0;
+  var dequeueCount = 0;
+  //var count = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[enqueueCount] = value;
+    enqueueCount++;
   };
 
   someInstance.dequeue = function(){
+    if(enqueueCount-dequeueCount > 0){
+      var holder = storage[dequeueCount];
+      delete storage[dequeueCount];
+      dequeueCount++;
+      return holder; 
+    }
+
   };
 
   someInstance.size = function(){
+    return enqueueCount-dequeueCount;
   };
 
   return someInstance;
 };
+
+//enqueue("a");  {"0":"a"} count = 1 first = 0
+//enqueue("b");  {"0":"a", "1":"b"}  count = 2  first = 0
+//dequeue();  {"1":"b"} count = 1 after deleting the current first item, first = 1
+//enqueue("c");  we want {"1":"b", "2":"c"} but we are getting {"1":"c"}
