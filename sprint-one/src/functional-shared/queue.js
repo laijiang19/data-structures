0f5a9e1c4798046dc154ffb988f809d9954842ce
lain.lai.jiang@gmail.com <lain.lai.jiang@gmail.com>
2015-08-20 11:55:47 -0500

    Implemented and passed all tests on queue using shared functional instantiation

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,37 @@
+
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance =  {
+    storage : {},
+    enqueueCnt : 0,
+    dequeueCnt : 0
+  };
+
+  _.extend(someInstance, queueMethods);
+  console.log(someInstance);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.enqueueCnt] = value;
+    this.enqueueCnt++;
+
+  },
+  dequeue: function(){
+    if(this.enqueueCnt - this.dequeueCnt > 0){
+      var holder = this.storage[this.dequeueCnt];
+      delete this.storage[this.dequeueCnt];
+      this.dequeueCnt++;
+      return holder;
+    }
+  },
+  size: function(){
+    console.log(this.enqueueCnt - this.dequeueCnt);
+    return this.enqueueCnt - this.dequeueCnt;
+  }
+};
 
 
