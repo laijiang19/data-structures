eb54a40a00ace8f5415920c010ed1d540ed49d3b
lain.lai.jiang@gmail.com <lain.lai.jiang@gmail.com>
2015-08-21 12:53:56 -0500

    Implemented and passed all tests on linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,17 +3,43 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value){
+
+  list.addToTail = function(value){ 
+    var node = new Node(value);
+
+    if (list.head === null){
+      list.head = node;
+    } else {
+      var currentTail = list.tail;
+      currentTail.next = node; 
+    } 
+    list.tail = node;
   };
+
 
   list.removeHead = function(){
+    var oldhead = list.head;
+    list.head = oldhead.next;
+
+    return oldhead.value;
   };
+
 
   list.contains = function(target){
+    var current = list.head;
+    while (current !== null){
+      if (target === current.value){
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   };
-
+  
+  
   return list;
 };
+
 
 var Node = function(value){
   var node = {};
@@ -26,4 +52,5 @@ var Node = function(value){
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * addToTail() O(1); removeHead() O(1); contains() O(n);
  */
