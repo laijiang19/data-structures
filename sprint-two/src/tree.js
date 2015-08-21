c8869ad3b112c999c60158bc01b555fd1d3a045a
lain.lai.jiang@gmail.com <lain.lai.jiang@gmail.com>
2015-08-21 18:24:59 -0500

    Implmented and passed all tests on tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,27 +1,36 @@
 var Tree = function(value){
   var newTree = {};
-  newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.value = value;
+  _.extend(newTree,treeMethods);
+  newTree.children = [];
 
   return newTree;
 };
 
 
-
-
-
 var treeMethods = {};
 
-treeMethods.addChild = function(value){
 
+treeMethods.addChild = function(value){
+  var node = Tree(value);
+  this.children.push(node);
 };
+
 
 treeMethods.contains = function(target){
+  var found = false; 
+  
+  if(this.value === target){
+    return true;
+  }
 
+  for (var i=0; i<this.children.length; i++){
+    found = this.children[i].contains(target) || found;
+  }
+  
+  return  found; 
 };
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
