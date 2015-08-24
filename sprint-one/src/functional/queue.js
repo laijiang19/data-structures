c76a5f905fc3a484259ef456f7ca635931dc5ee9
lain.lai.jiang@gmail.com <lain.lai.jiang@gmail.com>
2015-08-24 10:12:58 -0500

    Modified var name and removed some comments

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,27 +3,25 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var enqueueCount = 0;
-  var dequeueCount = 0;
-
-  // Implement the methods below
+  var start = 0;
+  var end = 0;
 
   someInstance.enqueue = function(value){
-    storage[enqueueCount] = value;
-    enqueueCount++;
+    storage[end] = value;
+    end++;
   };
 
   someInstance.dequeue = function(){
     if(someInstance.size() > 0){
-      var holder = storage[dequeueCount];
-      delete storage[dequeueCount];
-      dequeueCount++;
+      var holder = storage[start];
+      delete storage[start];
+      start++;
       return holder; 
     }
   };
 
   someInstance.size = function(){
-    return enqueueCount-dequeueCount;
+    return end-start;
   };
 
   return someInstance;
